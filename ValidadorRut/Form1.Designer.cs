cf050d3b4f3fd1d56681ed5df42600997204292f
FranciscoArmijo <francisco.j.armijo@gmail.com>
2020-04-10 14:23:28 -0400

    Validador de Rut Clase programacion 2

---
+++ ValidadorRut/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ValidadorRut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese rut para validar: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(234, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "VALIDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "El digito correcto es:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(308, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 38);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Enabled = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 51);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "el rut debe ir sin puntos,\r\ncon guion y con digito verificador\r\n11111111-1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(621, 177);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ValidadorRut/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidadorRut
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string rut = textBox1.Text;
+            //se agrega 0 a menos digitos
+            if (rut.Length == 9) 
+            {
+                rut = "0" + rut;
+            }
+            //operaion para validar
+            //int n0, n1, n2, n3, n4, n5, n6, n7 = 0;
+            double suma = 0;
+            double divisionDec = 0;
+            int divisionEnt = 0;
+            double valorDec = 0;
+            double resta11 = 0;
+            int digito = 0;
+            string digitok;
+            bool rutvalido = false;
+
+            //se añade una arreglo para el rut y las constantes
+            int[] constantes = { 3, 2, 7, 6, 5, 4, 3, 2 };
+            int[] rutarreglo;
+            rutarreglo = new int[8];
+            for (int i= 0; i < 8; i++)
+            {
+                rutarreglo[i] = Int32.Parse(rut[i].ToString());
+                suma = suma + (rutarreglo[i] * constantes[i]);
+            }
+            /*
+            n0 = 3 * Int32.Parse(rut[0].ToString());
+            n1 = 2 * Int32.Parse(rut[1].ToString());
+            n2 = 7 * Int32.Parse(rut[2].ToString());
+            n3 = 6 * Int32.Parse(rut[3].ToString());
+            n4 = 5 * Int32.Parse(rut[4].ToString());
+            n5 = 4 * Int32.Parse(rut[5].ToString());
+            n6 = 3 * Int32.Parse(rut[6].ToString());
+            n7 = 2 * Int32.Parse(rut[7].ToString());
+            suma = n0 + n1 + n2 + n3 + n4 + n5 + n6 + n7;
+            */
+
+            divisionDec = suma/11;
+            divisionEnt = (int)divisionDec;
+            valorDec = divisionDec - divisionEnt;
+            resta11 = 11 - (11 * (valorDec));
+            //El problema se resuelve redondeando con 0 decimales para que llegue al nuemro entero mas cercano.
+            digito = (int)Math.Round(resta11, 0);
+            //Asignar valores para digitos verificadores 0 y k;
+            if (digito == 10)
+            {
+                digitok = "k";
+                label3.Text = digitok;
+            }
+            else
+                if (digito == 11)
+            {
+                digito = 0;
+                label3.Text = digito.ToString();
+            }
+            else
+            {
+                label3.Text = digito.ToString();
+            }
+            if (label3.Text == Convert.ToString(rut[9]))
+            {
+                MessageBox.Show("El digito verificador está correcto", "Codigo correcto", MessageBoxButtons.OK);
+                rutvalido = true;
+            }
+            else
+            {
+                MessageBox.Show("El digito verificador es incorrecto", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
