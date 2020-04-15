e1203b1a77c91e97a72fa576b6a43e22f1f87e40
FranciscoArmijo <francisco.j.armijo@gmail.com>
2020-04-15 18:51:05 -0400

    Agregar booleano

--- ValidadorRut/Form1.cs
+++ ValidadorRut/Form1.cs
@@ -16,8 +16,7 @@ namespace ValidadorRut
         {
             InitializeComponent();
         }
-       
-
+        public bool rutverificado = false;
         private void button1_Click(object sender, EventArgs e)
         {
             string rut = textBox1.Text;
@@ -27,7 +26,6 @@ namespace ValidadorRut
                 rut = "0" + rut;
             }
             //operaion para validar
-            //int n0, n1, n2, n3, n4, n5, n6, n7 = 0;
             double suma = 0;
             double divisionDec = 0;
             int divisionEnt = 0;
@@ -35,7 +33,6 @@ namespace ValidadorRut
             double resta11 = 0;
             int digito = 0;
             string digitok;
-            bool rutvalido = false;
 
             //se añade una arreglo para el rut y las constantes
             int[] constantes = { 3, 2, 7, 6, 5, 4, 3, 2 };
@@ -46,17 +43,6 @@ namespace ValidadorRut
                 rutarreglo[i] = Int32.Parse(rut[i].ToString());
                 suma = suma + (rutarreglo[i] * constantes[i]);
             }
-            /*
-            n0 = 3 * Int32.Parse(rut[0].ToString());
-            n1 = 2 * Int32.Parse(rut[1].ToString());
-            n2 = 7 * Int32.Parse(rut[2].ToString());
-            n3 = 6 * Int32.Parse(rut[3].ToString());
-            n4 = 5 * Int32.Parse(rut[4].ToString());
-            n5 = 4 * Int32.Parse(rut[5].ToString());
-            n6 = 3 * Int32.Parse(rut[6].ToString());
-            n7 = 2 * Int32.Parse(rut[7].ToString());
-            suma = n0 + n1 + n2 + n3 + n4 + n5 + n6 + n7;
-            */
 
             divisionDec = suma/11;
             divisionEnt = (int)divisionDec;
@@ -83,16 +69,27 @@ namespace ValidadorRut
             if (label3.Text == Convert.ToString(rut[9]))
             {
                 MessageBox.Show("El digito verificador está correcto", "Codigo correcto", MessageBoxButtons.OK);
-                rutvalido = true;
+                rutverificado = true;
+                this.BackColor = Color.Green;
             }
             else
             {
                 MessageBox.Show("El digito verificador es incorrecto", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                rutverificado = false;
+                this.BackColor = Color.Red;
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            if (rutverificado)
+            {
+                BackColor = Color.Green;
+            }
+            else
+            {
+                BackColor = Color.Red;
+            }
         }
     }
 }
